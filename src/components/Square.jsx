1d9975f96b1377b9dcd7b53f59e42a4b3571b06f
asif ahemmed <asifahemmed0@gmail.com>
2024-10-08 10:46:51 +0600

    Board and square created

---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+
+// eslint-disable-next-line react/prop-types
+const Square = ({value}) => {
+    return (
+        <button className="text-lg bg-white border border-gray-500 leading-9 m-1 h-12 w-12 ">{value}</button>
+    );
+};
+
+export default Square;
